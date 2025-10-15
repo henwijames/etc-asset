ffdb19cca549d99f952d6c9c2d84e8e3f51e1e54
Henry James Ribano <114987518+henwijames@users.noreply.github.com>
2025-10-15 09:00:00 +0400

    refactor: Adjust login page heading margin for improved layout

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,7 +3,7 @@
     <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
             <img src={{ asset('assets/etclogo.jpeg') }} alt="Your Company" class="mx-auto h-36 w-auto" />
-            <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight">Sign in to your account</h2>
+            <h2 class="mt-5 text-center text-2xl/9 font-bold tracking-tight">Sign in to your account</h2>
         </div>
 
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
